24e1c4d7e0cf36dbf4bd34d9fc7eae315173a553
sanlirio <sandro.lirio2@gmail.com>
2021-03-27 10:53:45 -0300

    Project created

---
+++ src/main/java/com/cursoweb/curso/CursoSpApplication.java
@@ -0,0 +1,13 @@
+package com.cursoweb.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursoweb/curso/CursoSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoweb.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
